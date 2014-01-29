b06bd718d189b1cfa6fa4d4c59a61271dab5228f
Echo Yu <echo@echoyu.com>
2014-01-29 14:41:08 -0500

    scroller bar added

--- src/action.js
+++ src/action.js
@@ -1,9 +1,6 @@
 $(function() {
 
-	var	maxWidth = 500,
-		maxHeight = 500,
-		pic = $('#pic_holder'),
-		originalCanvas = null,
+	var	pic = $('#pic_holder'),
 		filters = $('#filters li a'),
 		filter_holder = $('#filter_holder');
 
@@ -13,24 +10,39 @@ $(function() {
 		on:{
 			load: function(e, file){
 
-				//good for image file under 1mb way too slow
+				$('#info').hide();
+				var img = $('<img>').appendTo(pic);
+		
+				img.load(function(){
 
-				var img = $('<img>').appendTo(pic_holder);
-				Caman(img[0], function(){
-					this.resize({
-						height: 500,
-						width: 500,
+					//proportional resize the image
+					img.css({'max-width': '100%',
+						 'max-height': '100%',
+						});
 
-					});
-					this.render();
+					// console.log(this.width);
+					// console.log(this.height);
+						
+					//vertical align the image in center
+					if(this.height<500){
+						var top= (500 - this.height)/2;
+
+					img.css({
+						marginTop: top,
+						});
+					}
+
+					filter_holder.fadeIn();
+
+					// Trigger the default "normal" filter
+					filters.first().click();
+
 				});
-
-			
-				img.attr('src', e.target.result);
-
-
+				
+				img.attr('src', e.target.result);			
 			},
 
+			//be sure the file is image
 			beforestart: function(file){
 				return /^image/.test(file.type);
 			},
